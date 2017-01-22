78b5c24cdbabb6a6ca282f0b28faf22fca824fe0
Dujin Jeong <wjdenwls123@gmail.com>
2017-01-22 15:05:41 +0900

    feat: update tile manager
    add fun spawn tile

--- Assets/Scripts/TileManager.cs
+++ Assets/Scripts/TileManager.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class TileManager : MonoBehaviour {
 
+    public GameObject[] tilePrefabs;
+
+    public GameObject currentTile;
+
+    private static TileManager instance;
+
+    public static TileManager Instance
+    {
+        get {
+            if (instance == null)
+            {
+                instance = GameObject.FindObjectOfType<TileManager>();
+            }
+            return instance;
+        }
+    }
+
 	// Use this for initialization
 	void Start () {
-		
+        for (int i = 0; i < 10; i++)
+        {
+            SpawnTile();
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void SpawnTile()
+    {
+        int randomIndex = Random.Range(0, 2);
+
+        currentTile = (GameObject)Instantiate(tilePrefabs[randomIndex], 
+             currentTile.transform.GetChild(0)
+            .transform.GetChild(randomIndex).position, Quaternion.identity);
+    }
 }
+
