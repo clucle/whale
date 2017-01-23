94cbeb1881bb7c5cee9ea5811be7cb4345948509
Dujin Jeong <wjdenwls123@gmail.com>
2017-01-23 22:57:36 +0900

    feat: add camera
    follow player

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Transform player;       // Reference to the player's transform.
+
+
+    void Awake()
+    {
+        // Setting up the reference.
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    void FixedUpdate()
+    {
+        transform.position = player.position + new Vector3(5f, 5f, -5f);
+    }
+}
