eddc32d8ad511bbd8a9a50ddc06e331b3b5d33bf
Dujin Jeong <wjdenwls123@gmail.com>
2017-02-01 11:08:11 +0900

    feat: add start scene
    scene move start to game

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeToScene()
+    {
+        AutoFade.LoadLevel("whale", 0.5f, 0.5f, Color.black);
+        //Application.LoadLevel("game");
+    }
+}
